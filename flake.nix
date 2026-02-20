4ee0836d0ec2b9b791c1293b53e83531261109f5
AVR-GCC <ogoun.d@gmail.com>
2026-02-20 22:40:04 +0200

    First commit

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "Nixos Nexus";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.bar-nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.homeManager
+        {
+          home-manager.users.bar = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
