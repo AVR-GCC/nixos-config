5c2ddf58e5ff0d4cb620dd365f66697cef33c10d
AVR-GCC <ogoun.d@gmail.com>
2026-02-21 23:42:37 +0200

    Completion

--- home.nix
+++ home.nix
@@ -114,6 +114,21 @@
         enable = true;
         servers.nixd.enable = true;
       };
+      cmp = {
+        enable = true;
+        settings.sources = [
+          { name = "nvim_lsp"; }
+          { name = "buffer"; }
+          { name = "path"; }
+        ];
+        settings.mapping = {
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<Tab>" = "cmp.mapping.select_next_item()";
+          "<S-Tab>" = "cmp.mapping.select_prev_item()";
+          "<C-e>" = "cmp.mapping.abort()";
+        };
+      };
       lualine.enable = true;
       neo-tree.enable = true;
       lazygit.enable = true;
