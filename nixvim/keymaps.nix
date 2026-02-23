c503da28eb7d4756297d27e444ba8916a0242f04
AVR-GCC <ogoun.d@gmail.com>
2026-02-23 20:10:06 +0200

    Keymaps to own file

---
+++ nixvim/keymaps.nix
@@ -0,0 +1,93 @@
+{ ... }:
+
+[
+  {
+    mode = "n";
+    key = "<leader>e";
+    action = ":Neotree toggle<CR>";
+    options.desc = "Toggle file tree";
+  }
+  {
+    mode = "n";
+    key = "<leader>h";
+    action.__raw = ''
+        function()
+          vim.cmd("botright " .. math.floor(vim.o.lines * 0.3) .. "split")
+          vim.cmd("terminal")
+          vim.defer_fn(function()
+            vim.cmd("startinsert")
+          end, 1)
+        end
+        '';
+    options.desc = "Open horizontal terminal";
+  }
+  {
+    mode = "t";
+    key = "<C-w>";
+    action.__raw = ''
+        function()
+          vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n>", true, false, true), "n", false)
+        end
+        '';
+    options.desc = "Exit terminal mode";
+  }
+  {
+    mode = "n";
+    key = "<Tab>";
+    action = ":bnext<CR>";
+    options.desc = "Next buffer";
+  }
+  {
+    mode = "n";
+    key = "<S-Tab>";
+    action = ":bprev<CR>";
+    options.desc = "Previous buffer";
+  }
+  {
+    mode = "n";
+    key = "<leader>lg";
+    action = ":LazyGit<CR>";
+    options.desc = "Lazy git";
+  }
+  {
+    mode = "n";
+    key = "<Esc>";
+    action = ":noh<CR>";
+    options.desc = "Clear search highlights";
+  }
+  {
+    mode = "n";
+    key = "<leader>x";
+    action.__raw = ''
+          function()
+            local buffers = vim.fn.getbufinfo({ buflisted = 1 })
+            local is_empty = vim.fn.bufname() == ''' and vim.fn.getbufvar(vim.fn.bufnr(), '&modified') == 0
+            if is_empty and #buffers <= 1 then
+              return
+            elseif #buffers > 1 then
+              vim.cmd('bp | bd #')
+            else
+              vim.cmd('enew | bd #')
+            end
+            end
+            '';
+    options.desc = "Close tab";
+  }
+  {
+    mode = "n";
+    key = "<leader>ai";
+    action.__raw = ''
+          function()
+            local cwd = vim.fn.getcwd()
+            local check = vim.fn.system("tmux list-panes -a -F '#{pane_current_command}' | grep -c opencode")
+            check = check:gsub("%s+", "")
+            if check ~= "0" then
+              vim.fn.system("tmux list-panes -a -F '#{pane_id} #{pane_current_command}' | grep opencode | awk '{print $1}' | xargs tmux kill-pane -t")
+            else
+              vim.fn.system("tmux split-window -h -l 25% -c '" .. cwd .. "' 'opencode'")
+            end
+          end
+          '';
+    options.desc = "Toggle opencode tmux pane";
+  }
+]
