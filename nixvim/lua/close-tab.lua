44b99a714e6874fe3cfc3c4642608db68aa8f790
AVR-GCC <ogoun.d@gmail.com>
2026-02-26 12:07:51 +0200

    Close terminal buffer

--- nixvim/lua/close-tab.lua
+++ nixvim/lua/close-tab.lua
@@ -1,11 +1,15 @@
 function()
   local buffers = vim.fn.getbufinfo({ buflisted = 1 })
-  local is_empty = vim.fn.bufname() == '\'' and vim.fn.getbufvar(vim.fn.bufnr(), '&modified') == 0
+  local is_terminal = vim.bo.buftype == 'terminal'
+  local is_empty = vim.fn.bufname() == '' and vim.fn.getbufvar(vim.fn.bufnr(), '&modified') == 0
   if is_empty and #buffers <= 1 then
     return
   elseif #buffers > 1 then
-    vim.cmd('bp | bd #')
+    vim.cmd('bp | bd! #')
   else
-    vim.cmd('enew | bd #')
+    vim.cmd('enew | bd! #')
+  end
+  if is_terminal then
+    vim.cmd('close')
   end
 end
