569f14535683350a349e0fbe34c307eeccc84306
AVR-GCC <ogoun.d@gmail.com>
2026-03-02 22:12:22 +0200

    No authenticate postgres

--- configuration.nix
+++ configuration.nix
@@ -144,6 +144,12 @@
       ssl_cert_file = "/var/lib/postgresql/server.crt";
       ssl_key_file = "/var/lib/postgresql/server.key";
     };
+    authentication = pkgs.lib.mkOverride 10 ''
+      # type  database  user  address         auth-method
+      local   all       all                   trust
+      host    all       all   127.0.0.1/32    trust
+      host    all       all   ::1/128         trust
+    '';
   };
   systemd.services.postgresql.preStart = pkgs.lib.mkAfter ''
     if [ ! -f /var/lib/postgresql/server.crt ]; then
